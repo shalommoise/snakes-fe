2b86f2a72c8a9d1f5d55c0c8b321e4796c2af069
Shalommoise <mshalom689@gmail.com>
2021-04-08 13:24:41 +0100

    using getSingleGame by _id for testing and building purposes

--- src/components/singlePlayer/SinglePlayerGame.jsx
+++ src/components/singlePlayer/SinglePlayerGame.jsx
@@ -9,11 +9,19 @@ class SinglePlayerGame extends Component  {
     snake2: [],
     isLoading: true
   }
-   startGame = (player1, player2, snake2)=>{
-    api.postGame(player1, player2, snake2)
-    .then((game)=>{
-      const {snake1, food, points1, game_over, active,size} = game;
-      this.setState({snake1, food, points1, game_over, active,size, isLoading: false})
+  //get game by id
+  //move snake
+  // eat food, food eaten => get score
+  
+   startGame = (id, player1, player2, snake2)=>{
+    // api.postGame(player1, player2, snake2)
+    // .then((game)=>{
+    //   const {snake1, food, points1, game_over, active,size, _id} = game;
+    //   this.setState({snake1, food, points1, game_over, active,size, _id, isLoading: false})
+    // })
+    api.getSingleGame(id).then((game)=>{
+    const {snake1, food, points1, game_over, active,size, _id} = game;
+      this.setState({snake1, food, points1, game_over, active,size, _id, isLoading: false})
     })
   }
    splitSnake =(snake)=>{
@@ -31,7 +39,15 @@ class SinglePlayerGame extends Component  {
    }
   componentDidMount(){
     const {userName,player2, snake2} = this.state
-    this.startGame(userName,player2, snake2); 
+    // this.startGame(userName,player2, snake2); 
+    this.startGame('606ee9f0ae02d60015b13a79')
+  }
+  directions = {
+    up: {x: 0, y: -1},
+    down: {x:0, y: 1},
+    right: {x:1, y:0},
+    left: {x:-1, y:0}
+
   }
   render(){
         const {userName,points, size, snake1, food, isLoading} = this.state;
@@ -42,7 +58,7 @@ class SinglePlayerGame extends Component  {
     <div >
       <h3>Name: {userName}</h3>
       <h3>Score: {points} </h3>
-    
+   
 <div className="game">
 {this.array.map((pixel, index)=>{
 return  <GamePixel key={index} index={index} size={size} head={this.splitSnake(snake1)[0]} body={this.splitSnake(snake1)[1]} food={food}/>

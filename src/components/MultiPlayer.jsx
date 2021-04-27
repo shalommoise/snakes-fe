b9f9346ba0643ca27961dc89c0522efc992db55a
Shalommoise <mshalom689@gmail.com>
2021-04-27 13:37:13 +0100

    struct for multiplayer login

--- src/components/MultiPlayer.jsx
+++ src/components/MultiPlayer.jsx
@@ -6,6 +6,15 @@ class MultiPlayer extends Component {
       <div  className="frame">
          <h2>Multi-Player</h2>
         <Link to="/">Home</Link>
+        <div className="multi-game_options">
+          <div>Join random Game</div>
+           <div>Start your own Game</div>  
+        </div>
+   <div className="Copy_code_from_friend">
+     Enter Code from friend
+
+    </div>
+
       </div>
     );
   }
